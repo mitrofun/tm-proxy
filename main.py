ec07d6c0a29efc0692425f13545d3a6b13805a15
Dmitriy Shesterkin <dmitriy.shesterkin@gmail.com>
2021-05-18 10:51:51 +0300

    Fixed errors in the programs functionality

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+import re
 from typing import Union
 
 import httpx
 
 from bs4 import BeautifulSoup  # type: ignore
-from bs4.element import Doctype, NavigableString  # type: ignore
+from bs4.element import NavigableString, Tag  # type: ignore
 
 from fastapi import FastAPI, HTTPException, status
-from fastapi.responses import HTMLResponse
+from fastapi.responses import Response
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    source_url: str = 'https://habr.com/ru/'
+    source_url: str = 'https://habr.com/'
     local_url: str = 'http://0.0.0.0:8000/'
     word_len: int = 6
 
@@ -22,11 +23,14 @@ class Settings(BaseSettings):
 settings = Settings()
 
 
-async def fetch_content(url: str) -> str:
+ignore_tags = ('script', 'style', 'html')
+
+
+async def fetch_data(url: str) -> httpx.Response:
     try:
         async with httpx.AsyncClient() as client:
-            r = await client.get(url)
-            return r.text
+            res: httpx.Response = await client.get(url)
+            return res
     except httpx.ConnectTimeout:
         raise HTTPException(
             status_code=status.HTTP_408_REQUEST_TIMEOUT,
@@ -39,63 +43,77 @@ async def fetch_content(url: str) -> str:
         )
 
 
-def replace_link(link: str):
+def replace_url(link: str):
     if not link:
         return
     return link.replace(settings.source_url, settings.local_url)
 
 
 def can_be_replaced(word: str) -> bool:
-    list_of_exception_characters = ('.', ',', '!', '?', '™')
-    if len(word) != settings.word_len:
+    if '\n' in word or len(word) != settings.word_len:
         return False
-    for char in list_of_exception_characters:
-        if char in word:
-            return False
     return True
 
 
-def replace_word(word: str) -> str:
+def patch_word(word: str) -> str:
     if not can_be_replaced(word):
         return word
     return f'{word}™'
 
 
-def update_string(string: str) -> str:
-    words: list = string.split(' ')
-
+def modify_string(text: str) -> str:
+    words = re.split(r'(\W+)', text)
     for index, word in enumerate(words):
-        words[index] = replace_word(word)
+        words[index] = patch_word(word)
+    return ''.join(words)
 
-    return ' '.join(words)
+
+def modify_link(tag) -> None:
+
+    map_tag_name_to_attr = {
+        'a': 'href',
+        'use': 'xlink:href'
+    }
+
+    for tag_name, html_att in map_tag_name_to_attr.items():
+        if tag.name == tag_name:
+            tag.attrs[html_att] = replace_url(tag.attrs.get(html_att))
 
 
-async def update_text(node: Union[NavigableString, Doctype]) -> None:
+def modify_tag(tag: Union[Tag, NavigableString]) -> None:
 
-    if type(node) == Doctype:
+    if tag.name in ignore_tags:
         return
-    if node.name == 'a':
-        node.attrs['href'] = replace_link(node.attrs.get('href'))
-    if node.string:
-        node.string.replace_with(update_string(string=node.string))
 
-    if hasattr(node, 'children'):
-        for child in node.children:
-            await update_text(child)
+    modify_link(tag)
+
+    if tag.string and tag.children:
+
+        for child_tag in tag.children:
+            try:
+                modify_tag(child_tag)
+            # NavigableString objects have no attributes; only Tag objects have them.
+            except AttributeError:
+                child_tag.parent.string = modify_string(child_tag)
 
 
-async def update_html(text_page: str) -> str:
-    soup = BeautifulSoup(text_page, 'html.parser')
-    for node in soup.children:
-        await update_text(node)
+async def modify_html(html_content: bytes) -> str:
+    soup = BeautifulSoup(html_content, 'html5lib')
+    for tag in soup.find_all(name=True):
+        modify_tag(tag)
     return str(soup)
 
 
-async def get_content(url) -> str:
+async def get_content(url) -> tuple[Union[str, bytes], str]:
     prefix = settings.source_url
-    text_page = await fetch_content(f'{prefix}{url}')
-    updated_text = await update_html(text_page)
-    return updated_text
+    data = await fetch_data(f'{prefix}{url}')
+
+    # Modify only html
+    if 'text/html' in data.headers.get('content-type'):
+        updated_text = await modify_html(data.content)
+        return updated_text, data.headers.get('content-type')
+
+    return data.content, data.headers.get('content-type')
 
 
 app = FastAPI(
@@ -106,7 +124,7 @@ app = FastAPI(
 )
 
 
-@app.get('/{url:path}', response_class=HTMLResponse)
-async def proxy_pages(url: str):
-    content = await get_content(url)
-    return content
+@app.get('/{url:path}')
+async def proxy(url: str):
+    content, media_type = await get_content(url)
+    return Response(content=content, media_type=media_type)
